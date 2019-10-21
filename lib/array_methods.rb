2fe993a82a25d9df72ea17e45414d4d0c7d5825e
Grant Ferowich <g.ferowich@gmail.com>
2019-10-21 20:44:39 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0 
+  while i < array.length 
+    if array[i] == value_to_find
+      return i
+    end
+    i += 1
+  end    
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort.last
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort[0]
 end
